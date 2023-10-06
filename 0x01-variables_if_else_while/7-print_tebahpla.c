33f4475521b30d4cc52b0ec3ec434e21af91b9fc
razorpro9991 <wiggle.ek@gmail.com>
2023-10-06 13:53:13 -0700

    tasks

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the lowercase alphabet in reverse.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
 
 	putchar('\n');
