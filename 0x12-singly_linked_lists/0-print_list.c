59c445f4e3f1e6b342b0b8d6e72f5043183e8aa8
razorpro9991 <wiggle.ek@gmail.com>
2023-11-16 16:20:29 -0800

    tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,6 @@
  *
  * Return: The number of nodes in h.
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
@@ -21,9 +20,8 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		nodes++;
 		h = h->next;
